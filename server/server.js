8705420d7a7e11a2166249ab058f7aadd6b82c71
Elena Carey <theelenacarey@gmail.com>
2024-03-17 00:36:46 -0500

    Apollo is a bear

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-import { ApolloServer } from 'apollo-server-express'; 
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import cors from 'cors';
 import { typeDefs, resolvers } from './schema/index.js';
 import connectToDatabase from './config/connection.js';
+import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
+
+
 
 async function startServer() {
   await connectToDatabase();
@@ -14,16 +16,24 @@ async function startServer() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer: app })], // Add plugin
+    plugins: [
+      // Install a landing page plugin based on NODE_ENV
+      process.env.NODE_ENV === 'production'
+        ? ApolloServerPluginLandingPageProductionDefault({
+            graphRef: 'my-graph-id@my-graph-variant',
+            footer: false,
+          })
+        : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    ],
   });
 
   await server.start(); // Start the Apollo server
 
   server.applyMiddleware({ app });
 
-  app.listen({ port: 3001 }, () => {
+  app.listen({ port: 4000 }, () => {
     console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
-    
+
   });
 }
 
